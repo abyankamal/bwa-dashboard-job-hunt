684800358d863e0ccbee42ecf1691da6ffde0201
abyankamal <kamalmuhammad414@gmail.com>
2024-02-21 15:15:28 +0700

    slicing post job page

--- src/app/post-a-job/page.tsx
+++ src/app/post-a-job/page.tsx
@@ -1,6 +1,20 @@
+"use client"; 
+
+import CKEditor from "@/components/organisms/CKEditor";
+import FieldInput from "@/components/organisms/FieldInput";
+import InputBenefits from "@/components/organisms/InputBenefits";
+import InputSkills from "@/components/organisms/inputSkills";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import { JOBTYPES } from "@/constants";
 import { jobFormSchema } from "@/lib/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { FC } from "react"
+import { ArrowLeftIcon } from "lucide-react";
+import { FC, useEffect, useState } from "react"
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -9,18 +23,247 @@ interface PageJobPostProps {
 }
 
 const PostJobPage : FC<PageJobPostProps> = ({}) => {
+    const [editorLoaded, setEditorLoaded] = useState<boolean>(false);
     const form = useForm<z.infer<typeof jobFormSchema>>({
-        resolver: zodResolver(jobFormSchema),
-        defaultValues: {
-            requiredSkills: [],
-        }
-    });
+		resolver: zodResolver(jobFormSchema),
+		defaultValues: {
+			requiredSkills: [],
+		},
+	});
+
+    useEffect(() =>{
+        setEditorLoaded(true);
+    }, [])
 
     const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
         console.log(val);
     }
     return (
-        <div>Post Job Page</div>
+        <div>
+            <div className="inline-flex items-center gap-2 cursor-pointer hover:text-primary">
+                <ArrowLeftIcon className="m-7 h-7" />
+                <span className="text-2xl font-semibold">Post A Job</span>
+            </div>
+
+            <div className="my-5">
+				<div className="text-lg font-semibold">Basic Information</div>
+				<div className="text-gray-400">
+					List out your top perks and benefits.
+				</div>
+			</div>
+
+			<Separator />
+
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className=" mt-5 space-y-8">
+                    <FieldInput
+                    title="Job Title"
+                    subtitle="Job Titles Must Be Describe One Position">
+                        <FormField 
+                        name="roles"
+                        control={form.control}
+                        render={({ field }) => (
+                            <FormItem>
+                                    <FormControl>
+                                        <Input 
+                                        className="w-[450px]"
+                                        placeholder="e.g Software Engginer"
+                                        {...field} />
+                                    </FormControl>
+                                <FormMessage />
+                                <FormDescription>
+                                    At Least 80 Characters
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                        />
+                    </FieldInput>
+
+                    <FieldInput
+						title="Type of Employment"
+						subtitle="You can select multiple type of employment"
+					>
+						<FormField
+							control={form.control}
+							name="jobType"
+							render={({ field }) => (
+								<FormItem className="space-y-3">
+									<FormControl>
+										<RadioGroup
+											onValueChange={field.onChange}
+											defaultValue={field.value}
+											className="flex flex-col space-y-1"
+										>
+											{JOBTYPES.map(
+												(item: string, i: number) => (
+													<FormItem
+														key={item + i}
+														className="flex items-center space-x-3 space-y-0"
+													>
+														<FormControl>
+															<RadioGroupItem
+																value={item}
+															/>
+														</FormControl>
+														<FormLabel className="font-normal">
+															{item}
+														</FormLabel>
+													</FormItem>
+												)
+											)}
+										</RadioGroup>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Salary"
+						subtitle="Please specify the estimated salary range for the role."
+					>
+						<div className="w-[450px] flex flex-row justify-between items-center">
+							<FormField
+								control={form.control}
+								name="salaryFrom"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												className="w-full"
+												placeholder="$100"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<span className="text-center">To</span>
+							<FormField
+								control={form.control}
+								name="salaryTo"
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												className="w-full"
+												placeholder="$1000"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+						</div>
+					</FieldInput>
+
+                    <FieldInput
+						title="Categories"
+						subtitle="You can select job categories"
+					>
+						<FormField
+							control={form.control}
+							name="categoryId"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Select Job Categories</FormLabel>
+									<Select
+										onValueChange={field.onChange}
+										defaultValue={field.value}
+									>
+										<FormControl>
+											<SelectTrigger className="w-[450px]">
+												<SelectValue placeholder="Select Job Categories" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											{data?.map((item: CategoryJob) => (
+												<SelectItem
+													key={item.id}
+													value={item.id}
+												>
+													{item.name}
+												</SelectItem>
+											))}
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Required Skills"
+						subtitle="Add required skills for the job"
+					>
+						<InputSkills
+							form={form}
+							name="requiredSkills"
+							label="Add Skills"
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Job Descriptions"
+						subtitle="Job titles must be describe one position"
+					>
+						<CKEditor
+							form={form}
+							name="jobDescription"
+							editorLoaded={editorLoaded}
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Responsibilites"
+						subtitle="Outline the core responibilites of the position"
+					>
+						<CKEditor
+							form={form}
+							name="responsibility"
+							editorLoaded={editorLoaded}
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Who You Are"
+						subtitle="Add Your Preferred Canidates Qualifications"
+					>
+						<CKEditor
+							form={form}
+							name="whoYouAre"
+							editorLoaded={editorLoaded}
+						/>
+					</FieldInput>
+
+                    <FieldInput
+						title="Nice-To-Haves"
+						subtitle="Add nice-to-have skills and qualifications for the role to encourage a more diverse set of candidates to apply"
+					>
+						<CKEditor
+							form={form}
+							name="niceToHaves"
+							editorLoaded={editorLoaded}
+						/>
+					</FieldInput>
+
+					<FieldInput
+						title="Perks and Benefits"
+						subtitle="Encourage more people to apply by sharing the attractive rewards and benefits you offer your employees"
+					>
+						<InputBenefits form={form} />
+					</FieldInput>
+
+                    <div className="flex justify-end">
+						<Button size="lg">Do a Review</Button>
+					</div>
+                </form>
+            </Form>
+        </div>
     );
 }
 
--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -2,20 +2,38 @@ import { JOBTYPES } from "@/constants";
 import { z } from "zod";
 
 export const jobFormSchema = z.object({
-    roles: z.string({ required_error: "Job Title Is Required"}).min(3, {message: "Job Title Must Be 3 Characters"}),
-    jobType: z.enum(JOBTYPES, {
-        required_error: "You Need To Choose Job Type",
-    }),
-    salaryFrom: z.string({ required_error: "Salary From Is Required"}),
-    salaryTo: z.string({ required_error: "Salary To Is Required"}),
-    categoryId: z.string({ required_error: "You need select a category" }),
-    requiredSkills: z.string().array().nonempty({message: "Required Skill must be at least 1 skills"}),
-    jobDescription: z.string({ required_error: "Job Description Is Required"}).min(10, {message: "Job Description Must Be 10 Characters"}),
-    responsibility: z.string({ required_error: "Job Description Is Required"}).min(10, {message: "Job Description Must Be 10 Characters"}),
-    whoYouAre: z.string({ required_error: "Job Description Is Required"}).min(10, {message: "Job Description Must Be 10 Characters"}),
-    niceToHaves: z.string({ required_error: "Job Description Is Required"}).min(10, {message: "Job Description Must Be 10 Characters"}),
-    benefits: z.object({
-        benefit: z.string(),
-        description: z.string()
-    }).array().nonempty({ message: "Benefits must be at least 1 benefit"})
-})
+	roles: z
+		.string({ required_error: "Job Title is required" })
+		.min(3, { message: "Job Title must be at least 3 characters" }),
+	jobType: z.enum(JOBTYPES, {
+		required_error: "You need to select a job type",
+	}),
+	salaryFrom: z.string({ required_error: "Salary From is required" }),
+	salaryTo: z.string({ required_error: "Salary To is required" }),
+	categoryId: z.string({ required_error: "You need to select a category" }),
+	requiredSkills: z
+		.string()
+		.array()
+		.nonempty({ message: "Required Skill must be at least 1 skill" }),
+	jobDescription: z
+		.string({ required_error: "Job Description is required" })
+		.min(10, { message: "Job Description must be at least 10 characters" }),
+	responsibility: z
+		.string({ required_error: "Responsibilities is required" })
+		.min(10, {
+			message: "Responsibilities must be at least 10 characters",
+		}),
+	whoYouAre: z
+		.string({ required_error: "Who You Are is required" })
+		.min(10, { message: "Who You Are must be at least 10 characters" }),
+	niceToHaves: z
+		.string({ required_error: "Nice-To-Haves is required" })
+		.min(10, { message: "Nice-To-Haves must be at least 10 characters" }),
+	benefits: z
+		.object({
+			benefit: z.string(),
+			description: z.string(),
+		})
+		.array()
+		.nonempty({ message: "Benefits must be at least 1 benefit" }),
+});
