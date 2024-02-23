0674ab4ff164932a1a89c4fad89300536ef4e66c
abyankamal <kamalmuhammad414@gmail.com>
2024-02-23 17:51:01 +0700

    integration detail job

--- src/app/api/company/new-user/route.ts
+++ src/app/api/company/new-user/route.ts
@@ -3,17 +3,17 @@ import prisma from "../../../../../lib/prisma";
 import { hashPassword } from "@/lib/utils";
 
 export async function POST(request: Request) {
-  const data = await request.json();
+	const data = await request.json();
 
-  const hashedPassword = await hashPassword(data.password);
+	const hashedPassword = await hashPassword(data.password);
 
-  const result = await prisma.company.create({
-    data: {
-      name: data.name,
-      email: data.email,
-      password: hashedPassword,
-    },
-  });
+	const result = await prisma.company.create({
+		data: {
+			name: data.name,
+			email: data.email,
+			password: hashedPassword,
+		},
+	});
 
-  return NextResponse.json(result);
+	return NextResponse.json(result);
 }
--- src/components/organisms/Applicants/index.tsx
+++ src/components/organisms/Applicants/index.tsx
@@ -1,47 +1,52 @@
 import { FC } from "react";
+
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
-import { JOB_APPLICANT_COLUMNS, JOB_APPLICANT_DATA } from "@/constants";
+import { JOB_APPLICANT_COLUMNS } from "@/constants";
 import ButtonActionTable from "../ButtonActionTable";
+import { Applicant, User } from "@prisma/client";
 
-interface ApplicantsProps{}
+type applicantType = {
+  user: User | null;
+} & Applicant;
 
-const Applicants: FC<ApplicantsProps> = ({}) => {
-    return(
-        <Table>
-                <TableCaption>A list of your recent invoices.</TableCaption>
-                <TableHeader>
-                <TableRow>
-							{JOB_APPLICANT_COLUMNS.map(
-								(item: string, i: number) => (
-									<TableHead key={item + i}>{item}</TableHead>
-								)
-							)}
-							<TableHead>Action</TableHead>
-						</TableRow>
-                </TableHeader>
-                <TableBody>
-                    {JOB_APPLICANT_DATA.map(
-                        (item: any, i: number) => (
-                            <TableRow key={item.name + i}>
-                                <TableCell>{item.name}</TableCell>
-                                <TableCell>{item.appliedDate}</TableCell>
-								<TableCell>
-									<ButtonActionTable url="" />
-								</TableCell>
-                            </TableRow>
-                        )
-                    )}
-                </TableBody>
-            </Table>
-    );
+interface ApplicantsProps {
+  applicants: applicantType[] | undefined;
 }
+
+const Applicants: FC<ApplicantsProps> = ({ applicants }) => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {JOB_APPLICANT_COLUMNS.map((item: string, i: number) => (
+            <TableHead key={item + i}>{item}</TableHead>
+          ))}
+          <TableHead>Action</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {applicants && (
+          <>
+            {applicants.map((item: any, i: number) => (
+              <TableRow key={item.id + i}>
+                <TableCell>{item.user.name}</TableCell>
+                <TableCell>
+                  <ButtonActionTable url="" />
+                </TableCell>
+              </TableRow>
+            ))}
+          </>
+        )}
+      </TableBody>
+    </Table>
+  );
+};
 
 export default Applicants;
